1560fb074e0d3e2ed886a9f25dbf25f4db0597c6
Nadhif Thoriqi <nadhifthoriqii@gmail.com>
2025-12-02 17:18:55 +0700

    halaman home SIA USP

--- js/home.js
+++ js/home.js
@@ -1,20 +1,21 @@
 const header = document.getElementById("header")
 const profile = document.getElementById("profile")
+const NIMRandom = `${new Date().getFullYear()}${new Date().getMonth()}${new Date().getDate()}0${new Date().getDay()}`;
 const params = new URLSearchParams(window.location.search)
 
-if (parseFloat(params.get("nama_nim")) != NaN) {
+if (Number.isNaN(parseFloat(params.get("nama_nim")))) {
+    var profil = `
+        <p>Nama                  : ${params.get("nama_nim")}</p>
+        <p>Prodi           &nbsp;: Teknik Informatika</p>
+        <p>NIM &nbsp;&nbsp;&nbsp;: ${NIMRandom}</p>
+        ` 
+} else {
     var profil = `
         <p>Nama                  : Mahasiswa</p>
         <p>Prodi           &nbsp;: Teknik Informatika</p>
         <p>NIM &nbsp;&nbsp;&nbsp;: ${params.get("nama_nim")}</p>
     `
-} else {
-    var profil = `
-        <p>Nama                  : ${params.get("nama_nim")}</p>
-        <p>Prodi           &nbsp;: Teknik Informatika</p>
-        <p>NIM &nbsp;&nbsp;&nbsp;: 2025120100</p>
-    ` 
-}
+    }
 profile.innerHTML = profil
 header.innerHTML = `
     <a href="home.html?nama_nim=${params.get("nama_nim")}&password=" class="logo-placeholder"></a>

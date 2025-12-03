d51f98c53becd330ddc136d05e7d5f885a96251c
Nadhif Thoriqi <nadhifthoriqii@gmail.com>
2025-12-03 20:51:58 +0700

    optimalisasi home

--- js/home.js
+++ js/home.js
@@ -15,10 +15,16 @@ if (Number.isNaN(parseFloat(params.get("nama_nim")))) {
         <p>Prodi           &nbsp;: Teknik Informatika</p>
         <p>NIM &nbsp;&nbsp;&nbsp;: ${params.get("nama_nim")}</p>
     `
-    }
+}
+
 profile.innerHTML = profil
 header.innerHTML = `
     <a href="home.html?nama_nim=${params.get("nama_nim")}&password=${params.get("password")}" class="logo-placeholder"></a>
     <h1>HOME</h1>
-    <i class="fas fa-bell header-icon"></i>
+    <form action="test.html" method="get">
+        <input type="hidden" name="test" value="Notifikasi">
+        <button class="btn">
+            <i class="fas fa-bell header-icon"></i>
+        </button>
+    </from>
 `
